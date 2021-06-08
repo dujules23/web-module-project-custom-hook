a59065067d2fe590d446262baf7ad736cffc2ea5
Durrell Jules <dujules23@gmail.com>
2021-06-08 16:12:31 -0400

    Completed layout of useLocalStorage

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,10 +1,21 @@
-import React from "react";
+import { useState } from "react";
 
 
 const useLocalStorage = (key, initialValue) => {
-    const [storedValue, setStoredValue] = useState(() =>{
+    const [storedValue, setStoredValue] = useState(() => {
         const item = window.localStorage.getItem(key);
 
-        return item ? JSON.parse(item)
-    })
+        setValue = (value) => {
+            //save state
+            setStoredValue(value);
+            // Save to local storage 
+            window.localStorage.setItem(key, JSON.stringfy(value))
+        }
+
+        return item ? JSON.parse(item) : initialValue;
+    });
+
+    return [storedValue, setValue];
 }
+
+export default useLocalStorage;
